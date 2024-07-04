211175efa904a7d5913b156319b04340cb5e9fb5
pameladeysarkar2003 <153226113+pameladeysarkar2003@users.noreply.github.com>
2024-07-04 23:49:52 +0530

    Add files via upload

---
+++ quizproject.py
@@ -0,0 +1,34 @@
+print("Welcome to my computer quiz!")
+
+playing=input("Do you want to play?")
+
+if playing !="yes":
+    quit()
+
+    print("Okay!Let's play:"))
+
+    if answer=="central processing unit":
+        print('Correct!')
+     else:
+        print("Incorrect!")   
+
+      answer=input("what does GpU stand for?")
+     if answer=="graphics processing unit:"
+    print('Correct!')
+else:
+   print('Incorrect!') 
+
+   answer=input("What does RAM stand for?")
+   if answer=="random access memory":
+      print("Correct")
+   else:
+      print("Incoorect!")
+
+      answer=input("What does PSU stand for?")
+      if answer=="power supply":
+         print('Correct!')
+      else:
+         print("Incorrect!")
+
+         print("You got"+str(score)+"questions correct!")
+         print("You got "+str((score/4)*100)+"%.")
